cfa5b2f4824af6a0a82c17b602224037a05864d6
ajitsase <34818235+ajitsase@users.noreply.github.com>
2019-06-20 18:28:09 +0530

    created product ,product runner class and product repository interface . Write the yml file.

---
+++ src/main/java/com/app/repo/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.app.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.app.model.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+
+}
