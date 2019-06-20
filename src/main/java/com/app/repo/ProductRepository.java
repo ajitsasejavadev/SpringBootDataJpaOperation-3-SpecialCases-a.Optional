131324efcc286fcba693ad4c3826a95376edab41
ajitsase <34818235+ajitsase@users.noreply.github.com>
2019-06-20 20:28:48 +0530

    just check

--- src/main/java/com/app/repo/ProductRepository.java
+++ src/main/java/com/app/repo/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.app.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -10,4 +12,9 @@ import com.app.model.Product;
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
 
+	@Query("Select p from Product p where p.prodId =:id ")
+	public Product getData(Integer id);              //This may throw exception
+	
+	@Query("Select p from Product p where p.prodId=:id")
+	public Optional<Product> getInfo(Integer id);    //This avoid null pointer exception
 }
--- src/main/java/com/app/runner/ProductRunner.java
+++ src/main/java/com/app/runner/ProductRunner.java
@@ -1,9 +1,12 @@
 package com.app.runner;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.app.model.Product;
 import com.app.repo.ProductRepository;
 
 @Component
@@ -15,7 +18,22 @@ public class ProductRunner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-	
+		System.out.println("------1.Output-----------------------------------");
+		Product p =repo.getData(4);
+		System.out.println(p.getProdCode());  //This may thorw exception
+
+		
+		System.out.println("------2.Output-----------------------------------");
+		Optional<Product> p1=repo.getInfo(2);
+		if(p1. isPresent()) {
+
+			Product prd =p1.get();
+			System.out.println(prd.getProdCode());
+
+		}else {
+			System.out.println("NO DATA FOUND..");
+		}
+
 	}
 
 }
